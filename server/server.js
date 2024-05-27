935af6c510fd9c3f32a075bc088be04b85515f73
=maki7389 <vighneshmakireddy@gmail.com>
2024-05-27 13:24:22 +0530

    Added Auth files done usieng MERN

---
+++ server/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import connectDB from './db/connect.js';
+import cors from 'cors';
+import axios from 'axios';
+import userRoutes from './routes/userRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log(process.env.PORT);
+
+const app = express();
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+// const MONGODB_URI = process.env.MONGODB_URI;
+
+app.use('/api/users', userRoutes);
+
+app.get('/',(req,res)=>{
+    res.send("Server is ready!");
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server started on port ${PORT}`)
+});
